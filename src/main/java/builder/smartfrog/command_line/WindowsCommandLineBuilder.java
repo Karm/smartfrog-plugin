6a742213565ca97fcbfd84cd056e08c72dcf699a
Jakub Cechacek <jcechace@redhat.com>
2013-07-22 09:08:25 +0200

    2/4 SF for Windows: Windows commandline builder

--- src/main/java/builder/smartfrog/command_line/WindowsCommandLineBuilder.java
+++ src/main/java/builder/smartfrog/command_line/WindowsCommandLineBuilder.java
@@ -1,64 +1,122 @@
 package builder.smartfrog.command_line;
 
+import builder.smartfrog.RewriteRule;
 import builder.smartfrog.SmartFrogHost;
+import builder.smartfrog.SmartFrogInstance;
+import builder.smartfrog.util.Functions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author jcechace
  */
-
-// TODO: Implemet this
 public class WindowsCommandLineBuilder extends AbstractCommandLineBuilder implements CommandLineBuilder {
 
     public WindowsCommandLineBuilder(SmartFrogHost host) {
         super(host);
-        throw new RuntimeException("Not implemented yet");
     }
 
+    @Override
+    public String getIniPath() {
+        return getBuilder().isUseAltIni() ?
+                getBuilder().getSfIni() : getSfInstance().getPath() + "/bin/default.ini";
+    }
+
+    @Override
+    public String getSlaveSupportPath() {
+        return super.getSlaveSupportPath()
+                .replace("\\","/");
+    }
+
+    @Override
+    public String getSfInstancePath() {
+        return super.getSfInstancePath()
+                .replace("\\", "/");
+    }
+
+    @Override
+    public String getWorkspacePath() {
+        return applyRewriteRules(super.getWorkspacePath())
+                .replace("\\", "/");
+    }
+
+    @Override
+    public String exportMatrixAxes() {
+        return "";
+    }
+
+    @Override
     public String[] buildDaemonCommandLine() {
-        return new String[0];  //To change body of implemented methods use File | Settings | File Templates.
+        String hostName =  getHost().getName();
+        return new String[] {"bash", "-xe", getRunScript(), hostName, getSlaveRunScript(),
+                getSfInstancePath(), getSFClassPath(), getWorkspacePath(), getSlaveWorkspacePath()};
     }
 
     @Override
-    String getIniPath() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public String[] buildDeployCommandLine(String scriptPath, String componentName) {
+        // TODO: path transformation workaround
+        if (getWorkspacePath() == getSlaveWorkspacePath()) {
+            scriptPath = scriptPath.replaceFirst(super.getWorkspacePath(), getWorkspacePath());
+        } else {
+            scriptPath = scriptPath.replaceFirst(super.getWorkspacePath(), getSlaveWorkspacePath());
+        }
+
+        String hostName =  getHost().getName();
+        return new String[] {"bash", "-xe", getDeployScript(), hostName,  getSlaveDeployScript(),
+                getSfInstancePath(), getSFClassPath(), componentName, scriptPath,
+                getWorkspacePath(), getSlaveWorkspacePath()};
     }
 
     @Override
-    String getSupportPath() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public String[] buildDeployTerminateHookCommandLine() {
+        String path = getSlaveSupportPath() + "/" + SmartFrogInstance.SUPPORT_SCRIPT;
+        return  buildDeployCommandLine(path, "terminate-hook");
     }
 
     @Override
-    String getSfInstancePath() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public String[] buildStopDaemonCommandLine() {
+        String hostName =  getHost().getName();
+        return new String[] {"bash", "-xe", getStopScript(), hostName, getSlaveStopScript(),
+                getSfInstancePath(), getSFClassPath(), getWorkspacePath(), getSlaveWorkspacePath()};
     }
 
     @Override
-    String getRunScript() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public  String[] buildKillThemAllCommandLine() {
+        String hostName =  getHost().getName();
+        return new String[] { "bash", "-xe", getKillScript(), getSlaveKillScript(), hostName};
     }
 
     @Override
-    String getStopScript() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public String applyRewriteRules(String path) {
+        List<RewriteRule> rewriteRules = getSfInstance().getRewriteRules();
+        for (RewriteRule rule : rewriteRules) {
+                path = rule.apply(path, getRewriteVariableMap());
+        }
+        return path;
     }
 
-    @Override
-    String getDeployScript() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+
+    private Map<String, String> getRewriteVariableMap() {
+        Map<String, String> variables = new HashMap<String, String>();
+        variables.put("{WORKSPACE}", super.getWorkspacePath());
+
+        return variables;
     }
 
-    @Override
-    String getWorkspacePath() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
 
-    @Override
-    String exportMatrixAxes() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public String[] buildStopDaemonCommandLine(String host) {
-        return new String[0];  //To change body of implemented methods use File | Settings | File Templates.
+    public String  getSFClassPath() {
+        String supportPath = getSlaveSupportPath() + "/*;";
+        StringBuilder sfPath = new StringBuilder(supportPath);
+        String[] userHomes = getBuilder().getSfUSerHomes();
+        for (String home : userHomes) {
+            if (!home.isEmpty()){
+                sfPath.append(home + "/*").append(";");
+            }
+        }
+        // TODO: workaround. Space will ensure that parameter containing semicolon will be passed correctly to batch script
+        sfPath.append(" ");
+        return sfPath.toString();
     }
 }
